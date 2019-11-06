47906399e1d9dbd73b0f088d7baec4fe8552359f
edb-c <edb.cofresi@gmail.com>
2019-11-06 16:41:41 -0500

    change text

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,38 +1,46 @@
-import React, { Component } from "react";
-import Jumbotron from 'react-bootstrap/Jumbotron'; 
+import React, { Component } from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 import Image from 'react-bootstrap/Image';
-import Llc from '../assets/Llc.png';
-import '../assets/Home.css'
+import Ethel2 from '../assets/Ethel2.jpg';
+//import Llc from '../assets/Llc.png';
+import '../assets/Home.css';
 
 class Home extends Component {
-  render() {
-   return (
-  <div>
-    <Jumbotron fluid>
-   
-    <h1>Magnum Opus!</h1>
-      <Row>
-        <Col>
-        <p>         
-
-        <h4>This is the last project for the Flatiron School Web Development!</h4>
-          It has been an incredible journey to this point!  Thank you for taking time to visit this project.
-          It serves as a building block to more complex projects.
-        </p>
-        <p>
-          Please visit my website for contact info and other works! <br/>
-          Thank you!<br/>          
-        </p>
-        <Image src={Llc} className="Home-Llc" alt="Llc" rounded />
-        </Col>
-      </Row>
-
-    </Jumbotron>
-  </div>
-      );
-    }
-  }
+	render() {
+		return (
+			<div>
+				<Jumbotron fluid>
+					<h1>Magnum Opus!</h1>
+					<Row>
+						<Col>
+							<p>
+								<h4>
+									This is the last project for the Flatiron School Web
+									Development!
+								</h4>
+								It has been an incredible journey to this point! Thank you for
+								taking time to visit this project. It serves as a building block
+								to more complex projects.
+								<br />
+								<br />
+								Live, Learn, Code!
+								<br />
+								<Image
+									src={Ethel2}
+									className='Home-Ethel2'
+									alt='Ethel2'
+									rounded
+								/>
+								<br />
+							</p>
+						</Col>
+					</Row>
+				</Jumbotron>
+			</div>
+		);
+	}
+}
 export default Home;
